19cf375cae6b107b062fa84358bcb1042d3fa7a0
GeorgeGUS <georgegus1@gmail.com>
2019-04-06 19:33:22 +0300

    Updates

---
+++ precache-manifest.49f554ce6681eee66b5b9fc88769ee8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "166258587ecbaa452320",
+    "url": "/analog-off/static/css/main.839a7eaf.chunk.css"
+  },
+  {
+    "revision": "166258587ecbaa452320",
+    "url": "/analog-off/static/js/main.058c4f62.chunk.js"
+  },
+  {
+    "revision": "b8c2ab128b3d4ace4640",
+    "url": "/analog-off/static/js/runtime~main.90a200e1.js"
+  },
+  {
+    "revision": "c6ccffebd7a215536580",
+    "url": "/analog-off/static/js/2.67b335f7.chunk.js"
+  },
+  {
+    "revision": "cd533637aaf437a23f6f8372e2297200",
+    "url": "/analog-off/index.html"
+  }
+];
